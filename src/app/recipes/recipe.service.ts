1cac6ac2b636cc89411ac7f0a07f7830f048de40
BuReh <rehbu23@g-apps.monett.k12.mo.us>
2022-11-15 14:09:40 -0600

    Passing Ingredients from Recipes to the Shopping List (via a Service) completed

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,10 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
@@ -9,7 +12,7 @@ export class RecipeService {
     new Recipe(
       'Tasty Schnitzel',
       'A super tasty Schnitzel - just awesome',
-      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.jpeg',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
       [
         new Ingredient('Meat', 1),
         new Ingredient('French Fries', 20)
@@ -17,14 +20,20 @@ export class RecipeService {
     new Recipe(
       'Big Fat Burger',
       'What else do you need to say?',
-      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpeg',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
       [
         new Ingredient('Buns', 2),
         new Ingredient('Meat', 1)
       ])
   ];
 
+  constructor(private slService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
